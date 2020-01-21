513c9163614cbc14333aaf3490b6458ff50308c0
Olarn U <olarn.u@gmail.com>
2020-01-21 16:53:56 +0700

    Rename test to snake case to increase more readable.

--- GlideRoseRefactor.Tests/GlideRoseLogicTest.cs
+++ GlideRoseRefactor.Tests/GlideRoseLogicTest.cs
@@ -9,7 +9,7 @@ namespace GlideRoseRefactor.Tests
         private Program app = new Program();
 
         [Fact]
-        public void OnceTheSellByDateHasPassed_QualityDegradesTwiceAsFast()
+        public void Once_the_sell_by_Date_has_passed_Quality_degrades_twice_as_fast()
         {
             //Given
             app.Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 1, Quality = 1 } };
@@ -23,7 +23,7 @@ namespace GlideRoseRefactor.Tests
         }
 
         [Fact]
-        public void TheQualityOfAnItemIsNeverNegative()
+        public void The_quality_of_an_item_is_never_negative()
         {
             //Given
             app.Items = new List<Item> { new Item { Name = "+5 Dexterity Vest", SellIn = 1, Quality = 0 } };
@@ -33,6 +33,16 @@ namespace GlideRoseRefactor.Tests
 
             //Then
             Assert.Equal(0, app.Items[0].Quality);
+        }
+
+        [Fact]
+        public void AgedBrie_actually_increases_in_Quality_the_older_it_gets()
+        {
+            //Given
+
+            //When
+
+            //Then
         }
 
     }
