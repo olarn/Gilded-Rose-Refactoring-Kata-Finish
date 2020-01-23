bb2b0e508add1fcbccdf06b5471e63a8cc5148e1
Olarn U <olarn.u@gmail.com>
2020-01-23 16:17:19 +0700

    Add Sulfuras requirement into class's comment.

--- GlideRoseRefactor.Console/CalculatorStrategies/SulfurasCalculator.cs
+++ GlideRoseRefactor.Console/CalculatorStrategies/SulfurasCalculator.cs
@@ -1,12 +1,15 @@
 namespace GlideRoseRefactor.Console
 {
+    /// Note: "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
     public class SulfurasCalculator : GlideRoseCalculator
     {
+        // This rule never apply to Sulfuras
         protected override void SetMaxIfQualityGreaterThanMaximum(Item item)
         {
             item.Quality = item.Quality;
         }
 
+        // This rule never apply to Sulfuras
         protected override void CalculateQuality(Item item)
         {
             item.Quality = item.Quality;
