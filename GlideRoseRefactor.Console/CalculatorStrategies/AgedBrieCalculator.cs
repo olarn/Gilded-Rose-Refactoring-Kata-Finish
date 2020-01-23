9ab8aa070281f2f0c33123085eb83f6de448a7a3
Olarn U <olarn.u@gmail.com>
2020-01-23 16:20:43 +0700

    Add requirement into some classes.

--- GlideRoseRefactor.Console/CalculatorStrategies/AgedBrieCalculator.cs
+++ GlideRoseRefactor.Console/CalculatorStrategies/AgedBrieCalculator.cs
@@ -2,6 +2,7 @@ namespace GlideRoseRefactor.Console
 {
     public class AgedBrieCalculator : GlideRoseCalculator
     {
+        // ged Brie" actually increases in Quality the older it gets
         protected override void CalculateQuality(Item item)
         {
             item.Quality += ValueOf.StandardQuality;
--- GlideRoseRefactor.Console/CalculatorStrategies/BackstagePassesCalculator.cs
+++ GlideRoseRefactor.Console/CalculatorStrategies/BackstagePassesCalculator.cs
@@ -2,6 +2,12 @@ namespace GlideRoseRefactor.Console
 {
     public class BackstagePassesCalculator : GlideRoseCalculator
     {
+        /* Requirement :-
+           "Backstage passes", like aged brie, increases in Quality as it's SellIn value approaches; 
+            Quality increases by 2 when there are 10 days 
+                  or less and by 3 when there are 5 days 
+                  or less but Quality drops to 0 after the concert
+        */
         protected override void CalculateQuality(Item item)
         {
             item.Quality += ValueOf.StandardQuality;
